09978bd95dd077cfd56388e72c7188a14f0379c9
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2023-05-22 21:56:40 +0300

    Extract routes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ const express = require('express');
 
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
-const homeController = require('./controllers/homeController');
-const cubeController = require('./controllers/cubeController');
+const routes = require('./routes');
 
 const app = express();
 
@@ -12,10 +11,6 @@ const PORT = 5000;
 expressConfig(app);
 handlebarsConfig(app);
 
-app.use(homeController);
-app.use('/cubes', cubeController);
-app.get('*', (req, res) => {
-    res.redirect('/404');
-});
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
