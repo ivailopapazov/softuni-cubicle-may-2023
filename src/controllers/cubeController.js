ae1a4904d67db3c230e67a130ebcc052abccfa63
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2023-05-22 21:36:25 +0300

    Add 404 page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -26,6 +26,10 @@ router.post('/create', (req, res) => {
 
 router.get('/:cubeId/details', (req, res) => {
     const cube = cubeManager.getOne(req.params.cubeId);
+
+    if (!cube) {
+        return res.redirect('/404');
+    }
     
     res.render('details', { cube });
 });
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -11,4 +11,8 @@ router.get('/about', (req, res) => {
     res.render('about');
 });
 
+router.get('/404', (req, res) => {
+    res.render('404');
+});
+
 module.exports = router;
