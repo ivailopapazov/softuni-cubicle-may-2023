fb8383937062607b3075ef6eac259066dc76d3c8
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2023-05-22 20:47:53 +0300

    Add id generator

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,10 +1,11 @@
+const uniqid = require('uniqid');
 const cubes = [];
 
 exports.getAll = () => cubes.slice();
 
 exports.create = (cubeData) => {
     const newCube ={
-        id: cubes.length + 1,
+        id: uniqid(),
         ...cubeData,
     };
     
