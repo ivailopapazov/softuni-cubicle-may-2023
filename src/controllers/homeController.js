d17a387e6cd40855402dae6956610d196a55ec7e
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2023-05-22 21:53:18 +0300

    Search form remembers inputs

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
 
     const cubes = cubeManager.getAll(search, from, to);
 
-    res.render('index', { cubes });
+    res.render('index', { cubes, search, from, to });
 });
 
 router.get('/about', (req, res) => {
