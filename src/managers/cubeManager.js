2cd3e7d3c3bd65d592bb959dc82ebbbd7c797dc6
Denis Vasilev <vassdeniss@gmail.com>
2023-05-24 01:20:40 +0300

    Added URL slugs

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,18 +1,22 @@
 const uniqid = require('uniqid');
+const slugify = require('slugify');
+
 const cubes = [
     {
         id: '1n73sh8holhz66elc',
         name: 'Mirror Cube',
         description: 'A cool mirror cube',
         imageUrl: 'https://m.media-amazon.com/images/I/71TrvUl50OL.jpg',
-        difficultyLevel: 4
+        difficultyLevel: 4,
+        slug: 'mirror-cube',
     },
     {
         id: '2n73sh8holaz66elc',
         name: 'Rubic Classic',
         description: 'Evergreen',
         imageUrl: 'https://www.hpcwire.com/wp-content/uploads/2018/07/Rubiks_Cube_shutterstock_271810067-675x380.jpg',
-        difficultyLevel: 3
+        difficultyLevel: 3,
+        slug: 'rubic-classic',
     }
 ];
 
@@ -34,12 +38,25 @@ exports.getAll = (search, from, to) => {
     return result;
 };
 
-exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId);
+exports.getOne = (slug) => {
+    const slugged = slugify(slug, {
+        replacement: '-',
+        lower: true,
+        strict: true,
+    });
+
+    return cubes.find((cube) => cube.slug === slugged);
+};
 
 exports.create = (cubeData) => {
     const newCube = {
         id: uniqid(),
         ...cubeData,
+        slug: slugify(cubeData.name, {
+            replacement: '-',
+            lower: true,
+            strict: true,
+        }),
     };
 
     cubes.push(newCube);
