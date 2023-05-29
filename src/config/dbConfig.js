dbd3d5818d46c65fbd574952f48b477c5d5f5171
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2023-05-29 18:38:16 +0300

    Setup mongoose

---
+++ src/config/dbConfig.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const uri = 'mongodb://127.0.0.1:27017/cubicle-may-2023'
+
+async function dbConnect() {
+    await mongoose.connect(uri);
+}
+
+module.exports = dbConnect;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
+const dbConnect = require('./config/dbConfig');
 const routes = require('./routes');
 
 const app = express();
@@ -10,6 +11,10 @@ const PORT = 5000;
 
 expressConfig(app);
 handlebarsConfig(app);
+
+dbConnect()
+    .then(() => console.log('DB Connected successfully'))
+    .catch(err => console.log('DB error: ', err));
 
 app.use(routes);
 
