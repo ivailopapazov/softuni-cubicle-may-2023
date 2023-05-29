a3d8576f8c09d38652a4ce393dacf11b85d66693
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2023-05-29 20:32:17 +0300

    Add attach accessory page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -34,4 +34,8 @@ router.get('/:cubeId/details', async (req, res) => {
     res.render('details', { cube });
 });
 
+router.get('/:cubeId/attach-accessory', (req, res) => {
+    res.render('accessory/attach');
+});
+
 module.exports = router;
