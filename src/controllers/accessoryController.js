eade882817d2caca59790e2124c925e384eff155
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2023-05-29 20:14:16 +0300

    Add post action for create accessory

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -4,4 +4,12 @@ router.get('/create', (req, res) => {
     res.render('accessory/create');
 });
 
+router.post('/create', (req, res) => {
+    const body = req.body;
+
+    // TODO: add accessory data to db
+
+    res.redirect('/');
+});
+
 module.exports = router;
