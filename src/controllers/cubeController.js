00ed7dd8a848b8fccd4bf55b89015c98acf72852
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2023-05-29 20:47:57 +0300

    Show cube and accessories on attach page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const cubeManager = require('../managers/cubeManager');
+const accessoryManager = require('../managers/accessoryManager');
 
 router.get('/create', (req, res) => {
     res.render('create');
@@ -26,16 +27,19 @@ router.post('/create', async (req, res) => {
 
 router.get('/:cubeId/details', async (req, res) => {
     const cube = await cubeManager.getOne(req.params.cubeId).lean();
-
+    
     if (!cube) {
         return res.redirect('/404');
     }
-    
+
     res.render('details', { cube });
 });
 
-router.get('/:cubeId/attach-accessory', (req, res) => {
-    res.render('accessory/attach');
+router.get('/:cubeId/attach-accessory', async (req, res) => {
+    const cube = await cubeManager.getOne(req.params.cubeId).lean();
+    const accessories = await accessoryManager.getAll().lean();
+
+    res.render('accessory/attach', { cube, accessories });
 });
 
 module.exports = router;
