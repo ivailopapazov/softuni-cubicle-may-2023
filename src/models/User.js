7e733598e21cd472b33fc47e03a2b7e68b6a9e34
Georgi Yunchov <georgiiunchov@gmail.com>
2023-06-09 15:07:05 +0300

    Add error handling to the user model

--- src/models/User.js
+++ src/models/User.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Password is required'],
         validate: {
-            validator: function(value) {
+            validator: function (value) {
                 return /^[A-Za-z0-9]+$/.test(value);
             },
             message: `Invalid password characters!`
@@ -23,22 +23,30 @@ const userSchema = new mongoose.Schema({
 
 // TODO: make it work
 userSchema.path('username').validate(async (username) => {
-    const usernameCount = await mongoose.models.User.countDocuments({ username });
+    try {
+        const usernameCount = await mongoose.models.User.countDocuments({ username });
 
-    return !usernameCount;
+        return !usernameCount;
+    } catch (error) {
+        console.log(error);
+    }
 }, 'Username alredy exists');
 
 userSchema.virtual('repeatPassword')
-    .set(function(value) {
+    .set(function (value) {
         if (value !== this.password) {
             throw new Error('Passsword missmatch!');
         }
     });
 
-userSchema.pre('save', async function() {
-    const hash = await bcrypt.hash(this.password, 10);
+userSchema.pre('save', async function () {
+    try {
+        const hash = await bcrypt.hash(this.password, 10);
 
-    this.password = hash;
+        this.password = hash;
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 const User = mongoose.model('User', userSchema);
